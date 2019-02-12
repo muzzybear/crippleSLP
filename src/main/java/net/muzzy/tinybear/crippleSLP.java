0b7ec451c35b6f8b44dbb3d1e74707766fa5ffcf
Matti Nikki <muzzy@iki.fi>
2019-02-12 12:21:51 +0200

    Switching to PaperMC for better control of SLP response

--- src/main/java/net/muzzy/tinybear/crippleSLP.java
+++ src/main/java/net/muzzy/tinybear/crippleSLP.java
@@ -10,42 +10,42 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+import com.destroystokyo.paper.profile.PlayerProfile;
+
 public class crippleSLP extends JavaPlugin implements Listener {
 
-	private static crippleSLP plugin;
+  private static crippleSLP plugin;
 
-	@Override
-	public void onEnable() {
-		plugin = this;
-		plugin.getServer().getPluginManager().registerEvents(this, this);
-		plugin.saveDefaultConfig();
-	}
+  @Override
+  public void onEnable() {
+    plugin = this;
+    plugin.getServer().getPluginManager().registerEvents(this, this);
+    plugin.saveDefaultConfig();
+  }
 
-	@EventHandler
-	public void onJoin(PlayerJoinEvent event) {
-		Player player = event.getPlayer();
-		String addr = player.getAddress().getAddress().getHostAddress();
+  @EventHandler
+  public void onJoin(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    String addr = player.getAddress().getAddress().getHostAddress();
 
-		plugin.getConfig().set("cache." + addr + ".UUID", player.getUniqueId().toString());
-		plugin.getConfig().set("cache." + addr + ".Name", player.getName());
-		plugin.saveConfig();
-	}
+    plugin.getConfig().set("cache." + addr + ".UUID", player.getUniqueId().toString());
+    plugin.getConfig().set("cache." + addr + ".Name", player.getName());
+    plugin.saveConfig();
+  }
 
-	@EventHandler
-	public void onPing(ServerListPingEvent event) {
-		String addr = event.getAddress().getHostAddress();
+  @EventHandler
+  public void onPing(PaperServerListPingEvent event) {
+    String addr = event.getAddress().getHostAddress();
 
-		if(plugin.getConfig().isSet("cache." + addr + ".Name")) {
-			plugin.getServer().getConsoleSender().sendMessage("[SLP] Request from " + addr + " (" + plugin.getConfig().getString("cache." + addr + ".Name") + ")");
-		} else {
-			plugin.getServer().getConsoleSender().sendMessage("[SLP] Request from " + addr);
-		}
+    if(plugin.getConfig().isSet("cache." + addr + ".Name")) {
+      plugin.getServer().getConsoleSender().sendMessage("[SLP] Request from " + addr + " (" + plugin.getConfig().getString("cache." + addr + ".Name") + ")");
+    } else {
+      plugin.getServer().getConsoleSender().sendMessage("[SLP] Request from " + addr);
+    }
 
-		Iterator<Player> iterator = event.iterator();
-		while (iterator.hasNext()) {
-		Player player = iterator.next();
-			iterator.remove();
-		}
-	}
+    java.util.List<PlayerProfile> players = event.getPlayerSample();
+    players.clear();
+  }
 
 }
